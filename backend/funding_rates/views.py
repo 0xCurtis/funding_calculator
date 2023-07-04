a20e93ce8596b097fceb78e1d5616471d34d95ff
0xCurtis <0xCurtis@users.noreply.github.com>
2023-07-04 22:33:18 +0200

    push all

--- backend/funding_rates/views.py
+++ backend/funding_rates/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import FundingSerializer
+from .models import Funding
+
 
 # Create your views here.
+
+class FundingView(viewsets.ModelViewSet):
+    serializer_class = FundingSerializer
+    queryset = Funding.objects.all()
