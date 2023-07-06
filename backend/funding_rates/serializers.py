5532bd04fdb3a60137918ff4c97804d39037a4ca
N/Name <87293285+0xCurtis@users.noreply.github.com>
2023-07-07 01:30:25 +0200

    Delete serializers.py

--- backend/funding_rates/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Funding
-
-
-class FundingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Funding
-        fields = ('id', 'pair', 'description', 'rate')
